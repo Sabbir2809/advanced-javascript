c3bdf57396ef1af48b7b717cbbe3c011638a1d77
SABBIR <sabbirto13@gmail.com>
2021-02-02 19:03:31 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,30 @@
+// Null Vs Undefined, different ways you will get undefined
+
+function add(num1, num2){   
+    console.log(num1 + num2);
+    return 
+}
+const result = add(13, 82);
+console.log(result);
+
+
+function add(num1, num2){   
+    console.log(num1, num2);
+
+}
+const ans = add(13);
+console.log(ans);
+
+
+let pakhi;
+console.log(pakhi);
+
+const premik = {name: "smart dude", phone: 50048};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9]
+console.log(ages[5]);
+
